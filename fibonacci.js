5d2467291628994c2aad638831c0e63808e737d9
Luis Tamarez <luistamarez@hotmail.com>
2023-01-09 22:13:14 -0800

    Add test for fibonacci   * Add max array size constraint

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,13 @@
+// max safe sequence siaze that will yield a fibonacci number that is less than Number.MAX_SAFE_INTERGER
+MAX_SAFE_SEQUENCE_SIZE = 79
 
 const fibonacci = (sequenceSize, sequenceArr = [0, 1]) => {
   if (sequenceSize === 1 || sequenceSize === 2) {
     return sequenceSize === 1 ? sequenceArr.splice(0, 1) : sequenceArr;
-  } else if (sequenceSize < 1 || !Number.isInteger(sequenceSize)) {
+  } else if (sequenceSize < 1 || sequenceSize > MAX_SAFE_SEQUENCE_SIZE || !Number.isInteger(sequenceSize)) {
     const inputTypeNonIntergerErrMsg = "Can not perform calculation on non-intergers";
-    const inputTypeLessThanOne = "Can not Return an array of size 0";
-    return !Number.isInteger(sequenceSize) ? inputTypeNonIntergerErrMsg : inputTypeLessThanOne;
+    const inputTypeOutOfRange = `Can not Return an array of size ${sequenceSize}`;
+    return !Number.isInteger(sequenceSize) ? inputTypeNonIntergerErrMsg : inputTypeOutOfRange;
   }
   const fibNum = sequenceArr[sequenceArr.length - 1] + sequenceArr[sequenceArr.length - 2];
   sequenceArr.push(fibNum);
@@ -20,13 +22,15 @@ console.log(fibonacci(0))  // "Can not Return an array of size 0"
 console.log(fibonacci(1))  // [0]
 console.log(fibonacci(8))  // [0, 1, 1, 2, 3, 5, 8, 13]
 
+
+
 const testFibonacci = (fibonacci, ) => { 
-  // the first 50 fibonacci numbers   
-  const fibSequeneToFiftyNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049];
+  // the first 78 fibonacci numbers   
+  const fibSequeneToSenventyEightNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445, 225851433717, 365435296162, 591286729879, 956722026041,1548008755920, 2504730781961, 4052739537881, 6557470319842, 10610209857723,17167680177565, 27777890035288, 44945570212853, 72723460248141, 117669030460994, 190392490709135, 308061521170129, 498454011879264, 806515533049393, 1304969544928657, 2111485077978050, 3416454622906707, 5527939700884757, 8944394323791464];
 
   // error messages
   const inputTypeNonIntergerErrMsg = "Can not perform calculation on non-intergers";
-  const inputTypeLessThanOne = "Can not Return an array of size 0";
+  const inputTypeOutOfRange = "Can not Return an array of size";
 
   const testCases = [    
   ["a", inputTypeNonIntergerErrMsg],
@@ -35,9 +39,9 @@ const testFibonacci = (fibonacci, ) => {
   [2.5, inputTypeNonIntergerErrMsg],
   [.5, inputTypeNonIntergerErrMsg],
   [0.1, inputTypeNonIntergerErrMsg],
-  [0, inputTypeLessThanOne],
-  [-1, inputTypeLessThanOne],
-  [50, fibSequeneToFiftyNumbers]
+  [0, `${inputTypeOutOfRange} 0`],
+  [-1, `${inputTypeOutOfRange} -1`],
+  [MAX_SAFE_SEQUENCE_SIZE, fibSequeneToSenventyEightNumbers]
   ];
 
   for (const testCase of testCases) {
@@ -65,4 +69,3 @@ function arrayEquals(arr1, arr2) {
 }
 
 testFibonacci(fibonacci);
-
