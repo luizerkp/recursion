eb9dd2ac4ac4df9d7fbc8adb40939eb813e58de8
Luis Tamarez <luistamarez@hotmail.com>
2023-01-17 13:46:56 -0800

    Clean up styler and syntax

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,11 @@
 // max safe sequence siaze that will yield a fibonacci number that is less than Number.MAX_SAFE_INTERGER
-MAX_SAFE_SEQUENCE_SIZE = 79
+const MAX_SAFE_SEQUENCE_SIZE = 79;
 
 const fibonacci = (sequenceSize, sequenceArr = [0, 1]) => {
   if (sequenceSize === 1 || sequenceSize === 2) {
     return sequenceSize === 1 ? sequenceArr.splice(0, 1) : sequenceArr;
-  } else if (sequenceSize < 1 || sequenceSize > MAX_SAFE_SEQUENCE_SIZE || !Number.isInteger(sequenceSize)) {
+  }
+  if (sequenceSize < 1 || sequenceSize > MAX_SAFE_SEQUENCE_SIZE || !Number.isInteger(sequenceSize)) {
     const inputTypeNonIntergerErrMsg = "Can not perform calculation on non-intergers";
     const inputTypeOutOfRange = `Can not Return an array of size ${sequenceSize}`;
     return !Number.isInteger(sequenceSize) ? inputTypeNonIntergerErrMsg : inputTypeOutOfRange;
@@ -15,39 +16,50 @@ const fibonacci = (sequenceSize, sequenceArr = [0, 1]) => {
 };
 
 // sample error outputs
-console.log(fibonacci("a"));  // "Can not perform calculation on non-intergers"
-console.log(fibonacci(0))  // "Can not Return an array of size 0"
+console.log(fibonacci("a")); // "Can not perform calculation on non-intergers"
+console.log(fibonacci(0)); // "Can not Return an array of size 0"
 
 // sample outputs
-console.log(fibonacci(1))  // [0]
-console.log(fibonacci(8))  // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibonacci(1)); // [0]
+console.log(fibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
 
+function arrayEquals(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+}
 
-
-const testFibonacci = (fibonacci, ) => { 
-  // the first 79 fibonacci numbers   
-  const fibSequeneToSenventyNineNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445, 225851433717, 365435296162, 591286729879, 956722026041,1548008755920, 2504730781961, 4052739537881, 6557470319842, 10610209857723,17167680177565, 27777890035288, 44945570212853, 72723460248141, 117669030460994, 190392490709135, 308061521170129, 498454011879264, 806515533049393, 1304969544928657, 2111485077978050, 3416454622906707, 5527939700884757, 8944394323791464];
+const testFibonacci = (fibonacciFunc) => {
+  // the first 79 fibonacci numbers
+  const fibSequeneToSenventyNineNumbers = [
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657,
+    46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352,
+    24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903,
+    2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445,
+    225851433717, 365435296162, 591286729879, 956722026041, 1548008755920, 2504730781961, 4052739537881, 6557470319842,
+    10610209857723, 17167680177565, 27777890035288, 44945570212853, 72723460248141, 117669030460994, 190392490709135,
+    308061521170129, 498454011879264, 806515533049393, 1304969544928657, 2111485077978050, 3416454622906707,
+    5527939700884757, 8944394323791464,
+  ];
 
   // error messages
   const inputTypeNonIntergerErrMsg = "Can not perform calculation on non-intergers";
   const inputTypeOutOfRange = "Can not Return an array of size";
 
-  const testCases = [    
-  ["a", inputTypeNonIntergerErrMsg],
-  ["1", inputTypeNonIntergerErrMsg],
-  [true, inputTypeNonIntergerErrMsg],
-  [2.5, inputTypeNonIntergerErrMsg],
-  [.5, inputTypeNonIntergerErrMsg],
-  [0.1, inputTypeNonIntergerErrMsg],
-  [0, `${inputTypeOutOfRange} 0`],
-  [-1, `${inputTypeOutOfRange} -1`],
-  [MAX_SAFE_SEQUENCE_SIZE, fibSequeneToSenventyNineNumbers]
+  const testCases = [
+    ["a", inputTypeNonIntergerErrMsg],
+    ["1", inputTypeNonIntergerErrMsg],
+    [true, inputTypeNonIntergerErrMsg],
+    [2.5, inputTypeNonIntergerErrMsg],
+    [0.5, inputTypeNonIntergerErrMsg],
+    [0.1, inputTypeNonIntergerErrMsg],
+    [0, `${inputTypeOutOfRange} 0`],
+    [-1, `${inputTypeOutOfRange} -1`],
+    [MAX_SAFE_SEQUENCE_SIZE, fibSequeneToSenventyNineNumbers],
   ];
 
-  for (const testCase of testCases) {
+  testCases.forEach((testCase) => {
     const size = testCase[0];
     const expected = testCase[1];
-    const result = fibonacci(size);
+    const result = fibonacciFunc(size);
     let pass = false;
 
     if (Array.isArray(result)) {
@@ -61,11 +73,7 @@ const testFibonacci = (fibonacci, ) => {
     } else {
       console.log(`Test case passed for size = ${size}`);
     }
-  }
-}
-
-function arrayEquals(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
-}
+  });
+};
 
 testFibonacci(fibonacci);
--- mergeSort.js
+++ mergeSort.js
@@ -1,81 +1,81 @@
 /**
-  * Divide  : Divide array into two sub-arrays
-  * Conquer : Sort the two sub-arrays recursively using merge sort
-  * Combine : Merge the two sub-arrays into a sorted array
-  **/
+ * Divide  : Divide array into two sub-arrays
+ * Conquer : Sort the two sub-arrays recursively using merge sort
+ * Combine : Merge the two sub-arrays into a sorted array
+ * */
 
 // merge both sub-arrays
 const merge = (leftSubArray, rightSubArray) => {
   const leftSubArrayLength = leftSubArray.length;
   const rightSubArrayLength = rightSubArray.length;
-  const mergedArray = []
-    
+  const mergedArray = [];
+
   let leftIdx = 0;
   let rightIdx = 0;
 
   while (leftIdx < leftSubArrayLength && rightIdx < rightSubArrayLength) {
     // ***NOTE: for descending order change comparison to '>='
-    if (leftSubArray[leftIdx] < rightSubArray[rightIdx])
-    {
+    if (leftSubArray[leftIdx] < rightSubArray[rightIdx]) {
       mergedArray.push(leftSubArray[leftIdx]);
       leftIdx += 1;
-    }
-    else
-    {
+    } else {
       mergedArray.push(rightSubArray[rightIdx]);
       rightIdx += 1;
     }
-
   }
 
   const leftRemainder = leftSubArray.slice(leftIdx);
   const rightRemainder = rightSubArray.slice(rightIdx);
 
-  return [...mergedArray, ...leftRemainder, ...rightRemainder]
+  return [...mergedArray, ...leftRemainder, ...rightRemainder];
+};
+
+function arrayEquals(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
 }
 
 // recusive call mergeSort
 const mergeSort = (arr) => {
   if (!Array.isArray(arr)) {
-    return `mergeSort error: "arr" parameter must be an Array found ${typeof arr}`;
+    return `mergeSort error: parameter must be an Array found ${typeof arr}`;
   }
-  
+
   if (arr.length <= 1) {
-    return arr
+    return arr;
   }
 
   // find array mid point
-  const mid= Math.floor(arr.length/2);
+  const mid = Math.floor(arr.length / 2);
 
   const leftSubArray = arr.slice(0, mid);
   const rightSubArray = arr.slice(mid, arr.length);
 
   // Combine
   return merge(mergeSort(leftSubArray), mergeSort(rightSubArray));
-}
+};
 
 const arr = [2, 1, 3, 7, 6, 5, 0, 4, 9, 8];
 
 console.log(mergeSort(arr)); // expected [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const testMergeSort = (mergeSort, ) => { 
+const testMergeSort = (mergeSortFunc) => {
   const testArr1 = [];
   const testArr1Expected = [];
 
   const testArr2 = [2, 1, 3, 7, 6, 5, 0, 4, 9, 8];
-  const testArr2Expected = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const testArr2Expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const testArr3 = "Hello";
-  const testArr3Expected = `mergeSort error: "arr" parameter must be an Array found string`;
+  const testArr3Expected = "mergeSort error: parameter must be an Array found string";
 
-  const testArr4 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const testArr4Expected = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const testArr4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const testArr4Expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const testArr5 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-  const testArr5Expected = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const testArr5Expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const testArr6 = 899;
-  const testArr6Expected = `mergeSort error: "arr" parameter must be an Array found number`;
+  const testArr6Expected = "mergeSort error: parameter must be an Array found number";
 
   const testArr7 = ["a", "b", "c", "e"];
   const testArr7Expected = ["a", "b", "c", "e"];
@@ -93,10 +93,9 @@ const testMergeSort = (mergeSort, ) => {
   const testArr11Expected = [0, 1, 1, 1, 1, 1, 1, 1, 1];
 
   const testArr12 = true;
-  const testArr12Expected = `mergeSort error: "arr" parameter must be an Array found boolean`;
-  
-  
-  const testCases = [    
+  const testArr12Expected = "mergeSort error: parameter must be an Array found boolean";
+
+  const testCases = [
     [testArr1, testArr1Expected],
     [testArr2, testArr2Expected],
     [testArr3, testArr3Expected],
@@ -110,11 +109,10 @@ const testMergeSort = (mergeSort, ) => {
     [testArr11, testArr11Expected],
     [testArr12, testArr12Expected],
   ];
-
-  for (const testCase of testCases) {
+  testCases.forEach((testCase) => {
     const test = testCase[0];
     const expected = testCase[1];
-    const result = mergeSort(test);
+    const result = mergeSortFunc(test);
     let pass = false;
 
     if (Array.isArray(result)) {
@@ -128,11 +126,7 @@ const testMergeSort = (mergeSort, ) => {
     } else {
       console.log(`Test case passed for Test: ${test}`);
     }
-  }
-}
-
-function arrayEquals(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
-}
+  });
+};
 
 testMergeSort(mergeSort);
